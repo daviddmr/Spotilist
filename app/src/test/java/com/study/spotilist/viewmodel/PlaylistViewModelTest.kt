f287f4ed1717be67e1810fc384ea4b14fedf63ad
David Ribeiro <david.ribeiro@bookjane.com>
2023-04-05 19:10:36 -0400

    Implement PlaylistViewModel unit tests

--- app/src/test/java/com/study/spotilist/viewmodel/PlaylistViewModelTest.kt
+++ app/src/test/java/com/study/spotilist/viewmodel/PlaylistViewModelTest.kt
@@ -1,5 +1,62 @@
 package com.study.spotilist.viewmodel
 
-import org.junit.jupiter.api.Assertions.*
+import com.nhaarman.mockitokotlin2.whenever
+import com.study.spotilist.model.Playlist
+import com.study.spotilist.repository.PlaylistRepository
+import com.study.spotilist.utils.BaseUnitTest
+import com.study.spotilist.utils.getValueForTest
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito.*
+import java.lang.RuntimeException
 
-internal class PlaylistViewModelTest
+class PlaylistViewModelTest : BaseUnitTest() {
+
+    private val repository: PlaylistRepository = mock()
+    private val playlists: List<Playlist> = mock()
+
+    @Test
+    fun fetchPlaylistsShouldCallFetchAllPlaylistsFromRepository() {
+        runBlocking {
+            val sut = PlaylistViewModel(repository)
+            sut.fetchPlaylists()
+            verify(repository, times(1)).fetchAllPlaylist()
+        }
+    }
+
+    @Test
+    fun fetchAllPlaylistsShouldReturnSuccessInCaseOfSuccess() {
+        runBlocking {
+            val sut = mockSuccess()
+            sut.fetchPlaylists()
+            val valueForTest = sut.playlists.getValueForTest()
+            Assert.assertEquals(playlists, valueForTest)
+        }
+    }
+
+    @Test
+    fun fetchAllPlaylistsShouldReturnNullInCaseOfFailure() {
+        runBlocking {
+            val sut = mockFailure()
+            sut.fetchPlaylists()
+            val valueForTest = sut.playlists.getValueForTest()
+            Assert.assertEquals(null, valueForTest)
+        }
+    }
+
+    private suspend fun mockFailure(): PlaylistViewModel {
+        whenever(repository.fetchAllPlaylist()).thenThrow(
+            RuntimeException("Something went wrong")
+        )
+        return PlaylistViewModel(repository)
+    }
+
+    private suspend fun mockSuccess(): PlaylistViewModel {
+        whenever(repository.fetchAllPlaylist()).then {
+            playlists
+        }
+
+        return PlaylistViewModel(repository)
+    }
+}
