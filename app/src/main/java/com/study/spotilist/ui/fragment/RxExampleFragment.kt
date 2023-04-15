772cbed5612e79c8b169cd1eb3884a597c5576d5
David Ribeiro <david.ribeiro@bookjane.com>
2023-04-15 16:28:22 -0400

    Create RxExample fragment, viewmodel and layout

---
+++ app/src/main/java/com/study/spotilist/ui/fragment/RxExampleFragment.kt
@@ -0,0 +1,27 @@
+package com.study.spotilist.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.study.spotilist.databinding.FragmentRxExampleBinding
+import com.study.spotilist.viewmodel.RxExampleViewModel
+
+class RxExampleFragment : Fragment() {
+
+    private val viewModel: RxExampleViewModel by viewModels()
+    private lateinit var binding: FragmentRxExampleBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentRxExampleBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/study/spotilist/viewmodel/RxExampleViewModel.kt
@@ -0,0 +1,6 @@
+package com.study.spotilist.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class RxExampleViewModel : ViewModel() {
+}
