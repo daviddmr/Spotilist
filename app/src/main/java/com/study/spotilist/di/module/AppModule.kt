1314346342e48de270da160378a9a4702d189926
David Ribeiro <david.ribeiro@bookjane.com>
2023-04-16 12:59:56 -0400

    Add RxJava2CallAdapter library and to Retrofit instance

--- app/src/main/java/com/study/spotilist/di/module/AppModule.kt
+++ app/src/main/java/com/study/spotilist/di/module/AppModule.kt
@@ -8,6 +8,7 @@ import dagger.hilt.android.components.FragmentComponent
 import dagger.hilt.android.components.ViewModelComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
@@ -27,6 +28,7 @@ class AppModule {
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .client(okHttpClient)
         .baseUrl("http://localhost:3000")
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
