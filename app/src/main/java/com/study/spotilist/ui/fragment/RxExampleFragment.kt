c09dcaf877881bab1611d43c7de71b28c5434f25
David Ribeiro <david.ribeiro@bookjane.com>
2023-04-16 14:43:07 -0400

    Add example of using Rx Observable combined with click listener to the project

--- app/src/main/java/com/study/spotilist/ui/fragment/RxExampleFragment.kt
+++ app/src/main/java/com/study/spotilist/ui/fragment/RxExampleFragment.kt
@@ -1,6 +1,7 @@
 package com.study.spotilist.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +10,19 @@ import androidx.fragment.app.viewModels
 import com.study.spotilist.databinding.FragmentRxExampleBinding
 import com.study.spotilist.viewmodel.RxExampleViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class RxExampleFragment : Fragment() {
+
+    companion object {
+        const val TAG = "RxExampleFragment"
+    }
 
     private val viewModel: RxExampleViewModel by viewModels()
     private lateinit var binding: FragmentRxExampleBinding
@@ -24,6 +35,30 @@ class RxExampleFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.onRxTest()
+
+//        Observable.create { emitter ->
+//            binding.fragmentRxExampleButton.setOnClickListener {
+//                emitter.onNext(it)
+//            }
+//        }.subscribe {
+//            onButtonClicked()
+//        }
+
+        binding.fragmentRxExampleButton.clicks().debounce(4, TimeUnit.SECONDS).subscribe {
+            onButtonClicked()
+        }
+    }
+
+    fun View.clicks(): Observable<View> {
+        return Observable.create { emitter ->
+            this.setOnClickListener {
+                emitter.onNext(it)
+            }
+        }
+    }
+
+    fun onButtonClicked() {
+        Log.d(TAG, "Button clicked!!!")
     }
 
 }
